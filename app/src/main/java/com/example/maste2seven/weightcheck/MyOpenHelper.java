6d11f85911c65aedc77d5d589748462808e2c902
7six <maste2seven@gmail.com>
2016-02-03 10:29:38 +0700

    Crate Constrctor finish

--- app/src/main/java/com/example/maste2seven/weightcheck/MyOpenHelper.java
+++ app/src/main/java/com/example/maste2seven/weightcheck/MyOpenHelper.java
@@ -4,4 +4,11 @@ package com.example.maste2seven.weightcheck;
  * Created by Maste2seven on 3/2/2559.
  */
 public class MyOpenHelper {
-}
+
+    public MyOpenHelper() {
+
+
+
+    }//constrctor
+
+}// mainclass
