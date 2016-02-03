4d3acfab5f166e6cf359673e12770338e24c1a42
7six <maste2seven@gmail.com>
2016-02-03 11:03:25 +0700

    Crate MyOpenHelper Succus

--- app/src/main/java/com/example/maste2seven/weightcheck/MyOpenHelper.java
+++ app/src/main/java/com/example/maste2seven/weightcheck/MyOpenHelper.java
@@ -1,14 +1,39 @@
 package com.example.maste2seven.weightcheck;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by Maste2seven on 3/2/2559.
  */
-public class MyOpenHelper {
-
-    public MyOpenHelper() {
+public class MyOpenHelper extends SQLiteOpenHelper{
 
 
+    //explicit
+    public static final String database_name = "weight.db"; //ประกาศค่าคงที่ ไม่สามารถแก้ไขได้
+    private static final int database_version = 1;
+    private static final String table_weight = "create table weightTable(" +
+            "_id integer primary key, " +
+            "Date text, " +
+            "weight text);";
+
+
+
+
+    public MyOpenHelper(Context context) {
+    super(context,database_name,null,database_version);
+    //ทำการเชื่อมต่อ ค้นหาชื่อและตรวจสอบเวอร์ชั่น
 
     }//constrctor
 
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(table_weight);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+
+    }
 }// mainclass
