0fb2d0b562f3a7b0d467d10264746a10b1873270
7six <maste2seven@gmail.com>
2016-02-03 11:35:21 +0700

    Crate SQLlite Successfull

--- app/src/main/java/com/example/maste2seven/weightcheck/MainActivity.java
+++ app/src/main/java/com/example/maste2seven/weightcheck/MainActivity.java
@@ -5,9 +5,17 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    //expicit
+    private MyManage objMyManage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //requestdatabase
+        objMyManage = new MyManage(this);
+
+    }//main method
+}//main class
