2e4aab8fa3afa209f14ed3447aaa3c09b604d88e
7six <maste2seven@gmail.com>
2016-02-10 12:05:53 -0800

    Regis Finish

--- app/src/main/java/com/example/maste2seven/weightcheck/MyManage.java
+++ app/src/main/java/com/example/maste2seven/weightcheck/MyManage.java
@@ -1,5 +1,6 @@
 package com.example.maste2seven.weightcheck;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
@@ -12,6 +13,15 @@ public class MyManage {
     private MyOpenHelper objMyOpenHelper;
     private SQLiteDatabase writeSqLiteDatabase, readSqLiteDatabase; //คำสั่งให้เขียนและอ่าน
 
+    public static final String table_User = "userTable";
+    public static final String table_Weight = "weightTable";
+    public static final String colum_id = "id";
+    public static final String colum_user = "user";
+    public static final String colum_password = "password";
+    public static final String colum_Name = "name";
+    public static final String colum_Date = "date";
+    public static final String colum_weight = "weight";
+
     public MyManage(Context context) {
 
         //create & connected
@@ -20,6 +30,16 @@ public class MyManage {
         readSqLiteDatabase = objMyOpenHelper.getReadableDatabase();
 
     }//construtoc
+    //add new value
+    public long addUserTable(String strUser,String strPassword,String strName) {
+
+        ContentValues objContentValues = new ContentValues();
+        objContentValues.put(colum_user,strUser);
+        objContentValues.put(colum_password,strPassword);
+        objContentValues.put(colum_Name,strName);
+
+        return writeSqLiteDatabase.insert(table_User, null, objContentValues);
+    }
 
 }//main class
 
--- app/src/main/java/com/example/maste2seven/weightcheck/RegisterActivity.java
+++ app/src/main/java/com/example/maste2seven/weightcheck/RegisterActivity.java
@@ -2,11 +2,17 @@ package com.example.maste2seven.weightcheck;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Created by User on 10/2/2016.
  */
 public class RegisterActivity extends AppCompatActivity {
+    //explicit
+    private EditText userText,passwordText, nameText;
+    private String userString,passwordString, nameString;
 
 
     protected void onCreate(Bundle saveInstanceState) {
@@ -14,7 +20,42 @@ public class RegisterActivity extends AppCompatActivity {
             super.onCreate(saveInstanceState);
             setContentView(R.layout.activity_register);
 
+        //bind widget
+
+        bindWidget();
+
+
+    }//main method
+    private void bindWidget() {
+        userText = (EditText) findViewById(R.id.editText);
+        passwordText = (EditText) findViewById(R.id.editText2);
+        nameText = (EditText) findViewById(R.id.editText3);
 
     }
 
-}
+    public void clcikRegist(View view) {
+
+        userString = userText.getText().toString().trim();
+        passwordString = passwordText.getText().toString().trim();
+        nameString = nameText.getText().toString().trim();
+
+        if (userString.equals("")  || passwordString.equals("")  || nameString.equals("")) {
+
+            //have space
+            Toast.makeText(RegisterActivity.this,getResources().getString(R.string.have_space),
+                    Toast.LENGTH_SHORT).show();
+
+        } else {
+
+            //no have space
+            MyManage objMyManage = new MyManage(this);
+            objMyManage.addUserTable(userString, passwordString, nameString);
+            Toast.makeText(RegisterActivity.this,
+                    getResources().getString(R.string.success_regis),
+                    Toast.LENGTH_SHORT).show();
+
+
+        }  //if else statedment
+
+    }//clickregist
+}//main class
